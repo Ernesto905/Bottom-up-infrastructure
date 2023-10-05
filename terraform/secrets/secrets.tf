05a923796747df637dda4a3448c8ad809ee0d2d7
ernesto905 <ernesto90643@gmail.com>
2023-10-05 19:03:10 -0400

    Configured IAC approach to secrets as well as wrote some code for RDS

---
+++ terraform/secrets/secrets.tf
@@ -0,0 +1,15 @@
+// Creates a random string and initializes an entry into AWS Secrets manager
+resource "random_password" "master"{
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
+resource "aws_secretsmanager_secret" "password" {
+  name = "db-password"
+}
+
+resource "aws_secretsmanager_secret_version" "password" {
+  secret_id = aws_secretsmanager_secret.password.id
+  secret_string = random_password.master.result
+}
