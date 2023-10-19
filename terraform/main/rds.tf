1c3cffa8d17df4a9954a1dcf49f87cb17385be18
ernesto905 <ernesto90643@gmail.com>
2023-10-19 18:06:49 -0400

    Small changes to postgres configuration, like version (AWS isn't up to date, V16 yet), also changed storage to 100Gi

--- terraform/main/rds.tf
+++ terraform/main/rds.tf
@@ -15,13 +15,13 @@ module "rds" {
 
   identifier = local.db_name 
   engine               = "postgres"
-  engine_version       = "16"
-  family               = "postgres16" # DB parameter group
-  major_engine_version = "16"         # DB option group
+  engine_version       = "15"
+  family               = "postgres15" # DB parameter group
+  major_engine_version = "15"         # DB option group
   instance_class       = "db.m5d.large"
 
-  allocated_storage     = 1 
-  max_allocated_storage = 75 
+  allocated_storage     = 100 
+  max_allocated_storage = 100 
 
   username = "dbadmin"
   password = data.aws_secretsmanager_secret_version.password.id
